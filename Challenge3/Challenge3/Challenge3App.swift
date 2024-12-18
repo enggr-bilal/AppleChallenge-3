54cc5c5918c2b50c351f442019c87302b7fa9d24
enggr-bilal <engr.muhammadbilal97@gmail.com>
2024-12-18 17:11:46 +0100

    push project

---
+++ Challenge3/Challenge3/Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge3App.swift
+//  Challenge3
+//
+//  Created by Muhammad Bilal on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
